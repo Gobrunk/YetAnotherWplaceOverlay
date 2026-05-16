312eccab7a19a87e2a05bdaa3febe83a46ac0f5f
Gobrunk <krmr.francois@gmail.com>
2026-05-16 17:28:03 +0200

    feat: rename to Yet Another Wplace Overlay, bump version to 1.0.0, add auto-update URLs

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,14 @@ import terser from '@rollup/plugin-terser';
 const BANNER = `// ==UserScript==
 // @name            Yet Another Wplace Overlay
 // @name:en         Yet Another Wplace Overlay
-// @version         0.1.0
+// @version         1.0.0
 // @description     A userscript to enhance the user experience on Wplace.live. This includes, but is not limited to: uploading images to display locally on a canvas, adding a button to move the Wplace color palette menu, and other QoL features.
 // @description:en  A userscript to enhance the user experience on Wplace.live. This includes, but is not limited to: uploading images to display locally on a canvas, adding a button to move the Wplace color palette menu, and other QoL features.
 // @author          Gobrunk
 // @license         MPL-2.0
 // @match           https://wplace.live/*
+// @updateURL       https://raw.githubusercontent.com/Gobrunk/YetAnotherWplaceOverlay/refs/heads/master/tampermonkey.js
+// @downloadURL     https://raw.githubusercontent.com/Gobrunk/YetAnotherWplaceOverlay/refs/heads/master/tampermonkey.js
 // @grant           GM_addStyle
 // @grant           GM.setValue
 // @grant           GM_getValue
