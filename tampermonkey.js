4998e2d4e80acc4b33a55602ccb266ea4e898e28
Gobrunk <krmr.francois@gmail.com>
2026-06-05 19:31:33 +0200

    chore: release v1.15.0

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Yet Another Wplace Overlay
 // @name:en         Yet Another Wplace Overlay
-// @version         1.14.0
+// @version         1.15.0
 // @description     A userscript to enhance the user experience on Wplace.live. This includes, but is not limited to: uploading images to display locally on a canvas, adding a button to move the Wplace color palette menu, and other QoL features.
 // @description:en  A userscript to enhance the user experience on Wplace.live. This includes, but is not limited to: uploading images to display locally on a canvas, adding a button to move the Wplace color palette menu, and other QoL features.
 // @author          Gobrunk
